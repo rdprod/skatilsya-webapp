f17c6444275b75ef63b953cacbfe6743f747ed66
rdprod <rdvorin55@gmail.com>
2021-01-31 13:55:58 +0300

    Initial commit

---
+++ src/main/java/com/rdprod/springboot/spring_rdprod_webapp/SpringRdprodWebappApplication.java
@@ -0,0 +1,13 @@
+package com.rdprod.springboot.spring_rdprod_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRdprodWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRdprodWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rdprod/springboot/spring_rdprod_webapp/SpringRdprodWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdprod.springboot.spring_rdprod_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRdprodWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
