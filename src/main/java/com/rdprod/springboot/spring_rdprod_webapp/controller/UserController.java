46dab4bc305ddb5c979bf219d58777105c686b31
rdprod <rdvorin55@gmail.com>
2021-02-07 22:38:47 +0300

    fix auto authorization

--- src/main/java/com/rdprod/springboot/spring_rdprod_webapp/controller/UserController.java
+++ src/main/java/com/rdprod/springboot/spring_rdprod_webapp/controller/UserController.java
@@ -6,12 +6,10 @@ import com.rdprod.springboot.spring_rdprod_webapp.entity.User;
 import com.rdprod.springboot.spring_rdprod_webapp.service.role.RoleService;
 import com.rdprod.springboot.spring_rdprod_webapp.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,8 +44,9 @@ public class UserController {
 
     @PostMapping("/registrationProcess")
     public String registerProcess(@ModelAttribute("user") User user, HttpServletRequest request) {
+        String originalPassword = user.getPassword();
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        String encodedPassword = passwordEncoder.encode(originalPassword);
         user.setPassword(encodedPassword);
 
         Role simpleRole = roleService.findSimpleUserRole();
@@ -56,9 +55,9 @@ public class UserController {
         userService.saveNewUser(user);
 
         try {
-            request.login(user.getUsername(), user.getPassword());
+            request.login(user.getUsername(), originalPassword);
         } catch (ServletException exp) {
-            System.out.println("Ошибка автоматической авторизации - " + exp);
+            System.out.println("Ошибка автоматической аутентификации - " + exp);
         }
 
         return "redirect:/";
